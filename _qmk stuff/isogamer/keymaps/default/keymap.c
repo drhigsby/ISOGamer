0ea2a36bf199affd083ba2a05fb051bd33a9e9c0
drhigsby <doctorhigsby@gmail.com>
2021-10-28 11:14:23 -0700

    fixing compile errors

--- _qmk stuff/isogamer/keymaps/default/keymap.c
+++ _qmk stuff/isogamer/keymaps/default/keymap.c
@@ -33,12 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    ),
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_VOLU);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_VOLD);
         }
     }
+    return true;
 }
